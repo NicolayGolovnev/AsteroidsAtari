aa7289a9ec68b6b06b728079f1653ffd06fd33f3
Nicolay Golovnev <kolya.golovnev@mail.ru>
2019-11-21 00:17:06 +0700

    Creating a Release project

--- main.cpp
+++ main.cpp
@@ -1,14 +1,18 @@
 #include <SFML/Graphics.hpp>
 using namespace sf;
 
-const int W = 1200;
-const int H = 800;
+const int W = 1920;
+const int H = 1080;
 int main()
 {
     srand(time(0));
 
     RenderWindow app(VideoMode(W, H), "Asteroids!");
     app.setFramerateLimit(60);
+
+	while (app.isOpen()) {
+
+	}
 
     return 0;
 }
